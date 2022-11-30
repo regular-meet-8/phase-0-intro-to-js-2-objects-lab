a8edfa61bdad367797bc20c2f67fb18f68b27ef0
regular-meet-8 <eddiekim444@gmail.com>
2022-11-30 10:04:12 +0300

    jsobjlab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+let employee = {
+    name : `Naivedias Cash`,
+    streetAddress : `12 Broadway`
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    let newObj = {...obj}
+    newObj[key] = value;
+return newObj;
+}
+const newEmployee = updateEmployeeWithKeyAndValue(employee, `StreetAddress`, `11 Broadway`)
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key ,value){
+obj[key] = value;
+return obj;
+}
+employee = destructivelyUpdateEmployeeWithKeyAndValue(employee, `StreetAddress`, `12 Broadway`)
+
+function deleteFromEmployeeByKey(obj, key, value){
+   obj = {...employee}
+    delete obj[key]
+    return obj;
+}
+function destructivelyDeleteFromEmployeeByKey(obj, key, value){
+delete obj[key];
+return obj
+
+}
